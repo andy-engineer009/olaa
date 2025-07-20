e0acb48e0ce07d048fbce9ba3b733672a50eb6ca
rohitchoudhary99 <rohit.choudhary@softradix.in>
2025-07-20 14:31:02 +0530

    latest code

--- src/app/(main-layout)/page.tsx
+++ src/app/(main-layout)/page.tsx
@@ -8,7 +8,7 @@ import UserRolePopup from "@/components/userRolePopup";
 
 const influencers = [ 
   {
-    id: "1",
+    id:1,
     name: "John Doe",
     image: "https://apollo.olx.in/v1/files/1mfuey8oh35o3-IN/image;s=300x600;q=60",
     isVerified: true,
@@ -22,7 +22,7 @@ const influencers = [
     isFeatured: true,
   },
   {
-    id: "1",
+    id: 2,
     name: "Andrii",
     image: "https://apollo.olx.in/v1/files/1mfuey8oh35o3-IN/image;s=300x600;q=60",
     isVerified: true,
@@ -33,7 +33,7 @@ const influencers = [
     isFeatured: true,
   },
   {
-    id: "1",
+    id: 3,
     name: "mak",
     image: "https://apollo.olx.in/v1/files/1mfuey8oh35o3-IN/image;s=300x600;q=60",
     isVerified: true,
@@ -43,7 +43,7 @@ const influencers = [
     startingPrice: 1000,
   },
   {
-    id: "1",
+    id: 4,
     name: "Sasha",
     image: "https://apollo.olx.in/v1/files/1mfuey8oh35o3-IN/image;s=300x600;q=60",
     isVerified: true,
@@ -53,7 +53,7 @@ const influencers = [
     startingPrice: 1000,
   },
   {
-    id: "1",
+    id: 5,
     name: "Sasha",
     image: "https://apollo.olx.in/v1/files/1mfuey8oh35o3-IN/image;s=300x600;q=60",
     isVerified: true,
@@ -63,7 +63,7 @@ const influencers = [
     startingPrice: 1000,
   },
   {
-    id: "1",
+    id: 6,
     name: "Sasha",
     image: "https://apollo.olx.in/v1/files/1mfuey8oh35o3-IN/image;s=300x600;q=60",
     isVerified: true,
@@ -73,7 +73,7 @@ const influencers = [
     startingPrice: 1000,
   },
   {
-    id: "1",
+    id: 7,
     name: "Sasha",
     image: "https://apollo.olx.in/v1/files/1mfuey8oh35o3-IN/image;s=300x600;q=60",
     isVerified: true,
@@ -83,7 +83,7 @@ const influencers = [
     startingPrice: 1000,
   },
   {
-    id: "1",
+    id: 8,
     name: "Sasha",
     image: "https://apollo.olx.in/v1/files/1mfuey8oh35o3-IN/image;s=300x600;q=60",
     isVerified: true,
@@ -93,7 +93,7 @@ const influencers = [
     startingPrice: 1000,
   },
   {
-    id: "1",
+    id: 9,
     name: "Sasha",
     image: "https://apollo.olx.in/v1/files/1mfuey8oh35o3-IN/image;s=300x600;q=60",
     isVerified: true,
