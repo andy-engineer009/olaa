15cae81d14742bf5cee7eee35f1793b69fc67e4f
rohitchoudhary99 <rohit.choudhary@softradix.in>
2025-07-25 12:50:49 +0530

    info how

---
+++ src/app/how/page.tsx
@@ -0,0 +1,9 @@
+import LocalStorageValueHandler from "@/components/localstroageValue-handler";
+
+export default function How() {
+  return (
+    <div>
+      <LocalStorageValueHandler />          
+    </div>
+  );
+}
--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -371,19 +371,19 @@ console.log('mak')
                   </svg>
                 </button>
               {/* )  */}
-              {/* // : (
-              //   <button className="w-full flex items-center justify-between p-3 rounded-lg hover:bg-blue-50 transition-colors" onClick={() => router.push('/login')}>
-              //     <div className="flex items-center gap-3">
-              //       <svg className="w-5 h-5 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              //         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
-              //       </svg>
-              //       <span className="text-blue-600 font-medium">Login</span>
-              //     </div>
-              //     <svg className="w-5 h-5 text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              //       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
-              //     </svg>
-              //   </button>
-              // )} */}
+             
+                 <button className="w-full flex items-center justify-between p-3 rounded-lg hover:bg-blue-50 transition-colors" onClick={() => router.push('/how')}>
+                   <div className="flex items-center gap-3">
+                     <svg className="w-5 h-5 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                     </svg>
+                     <span className="text-blue-600 font-medium">How it works</span>
+                   </div>
+                   <svg className="w-5 h-5 text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+                   </svg>
+                 </button>
+             
             </div>
           </div>
         </div>
