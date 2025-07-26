a208ac5cb7f228f39ca9f5f135a1c249ff217af1
rohitchoudhary99 <rohit.choudhary@softradix.in>
2025-07-26 17:14:08 +0530

    laetst code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
 import { Provider } from "@/store/Provider";
 import LocalStorageValueHandler from "@/components/localstroageValue-handler";
 
--- src/components/localstroageValue-handler.tsx
+++ src/components/localstroageValue-handler.tsx
@@ -1,16 +1,19 @@
 'use client';
+import { useEffect } from "react";
 import { setIsLoggedIn } from "@/store/userRoleSlice";
 import { useDispatch } from "react-redux";
 
 export default function LocalStorageValueHandler() {
     const dispatch = useDispatch();
-    const isLoggedIn: any = localStorage.getItem('isLoggedIn');
-    if (isLoggedIn == null || isLoggedIn == false) {
-        dispatch(setIsLoggedIn(false));
-    }else{
-        dispatch(setIsLoggedIn(true));
-    }
 
-    return <></>;
+    useEffect(() => {
+        const isLoggedIn = localStorage.getItem('isLoggedIn');
+        if (isLoggedIn == null || isLoggedIn === "false") {
+            dispatch(setIsLoggedIn(false));
+        } else {
+            dispatch(setIsLoggedIn(true));
+        }
+    }, [dispatch]);
+
+    return <div />;
 }
-            
